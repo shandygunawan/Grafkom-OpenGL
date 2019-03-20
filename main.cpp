ad968f36484ccedbef0e56a34fe0d4a99bb3507b
Shandy <shandy.gunawan98@gmail.com>
2019-03-20 08:55:31 +0700

    add white cartesian line

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <GLFW/glfw3.h>
 #include <stdlib.h>
 #include <stdio.h>
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
 static void error_callback(int error, const char* description)
 {
     fputs(description, stderr);
@@ -12,11 +16,12 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 }
 int main(void)
 {
+    // Setting Window
     GLFWwindow* window;
     glfwSetErrorCallback(error_callback);
     if (!glfwInit())
         exit(EXIT_FAILURE);
-    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Simple example", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -72,6 +77,19 @@ int main(void)
         glVertex3f(0.0f, 0.0f, 0.f);
         glVertex3f(0.154508497187474f, 0.475528258147577f, 0.f);
         glVertex3f(-0.404508497187474f, 0.293892626146237, 0.f);
+        glEnd();
+
+        // Vertical lines
+        glBegin(GL_LINES);
+        glColor3f(1.f, 1.f, 1.f); // WHITE
+        glVertex3f(0.0f, -480.0f, 0.0f);
+        glVertex3f(0.0f, 480.0f, 0.0f);
+        glEnd();
+
+        glBegin(GL_LINES);
+        glColor3f(1.f, 1.f, 1.f); // WHITE
+        glVertex3f(-640.0f, 0.0f, 0.0f);
+        glVertex3f(640.0f, 0.0f, 0.0f);
         glEnd();
 
         // glVertex3f(-0.6f, -0.4f, 0.f);
--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 all: main run
 
 main: main.cpp
-	g++ -o main main.cpp -I../include -L../lib -lglfw3 -lopengl32 -lgdi32
+	g++ -o main main.cpp -IC:\mingw-w64\x86_64-8.1.0-posix-seh-rt_v6-rev0\mingw64\include -L../lib -lglfw3 -lopengl32 -lgdi32
 
 run: main
 	main
