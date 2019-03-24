d0e603c4d5a581b1ae5187906a77aa75ee20e9a1
unknown <ntnicholasthie@gmail.com>
2019-03-24 13:01:58 +0700

    Updated pentagon with different colors for each vertex

--- Week2/Pentagon/main.cpp
+++ Week2/Pentagon/main.cpp
@@ -1,109 +1,167 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "shader.hpp"
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-
-static void error_callback(int error, const char* description)
-{
-    fputs(description, stderr);
-}
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
-}
 int main(void)
 {
-    // Setting Window
+    // Initialise GLFW
+	if( !glfwInit() )
+	{
+		fprintf( stderr, "Failed to initialize GLFW\n" );
+		getchar();
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Open a window and create its OpenGL context
     GLFWwindow* window;
-    glfwSetErrorCallback(error_callback);
-    if (!glfwInit())
-        exit(EXIT_FAILURE);
-    window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Grafkom-OpenGL Blue Pentagon", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        exit(EXIT_FAILURE);
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetKeyCallback(window, key_callback);
-    while (!glfwWindowShouldClose(window))
-    {
-        float ratio;
-        int width, height;
-        glfwGetFramebufferSize(window, &width, &height);
-        ratio = width / (float) height;
-        glViewport(0, 0, width, height);
-        glClear(GL_COLOR_BUFFER_BIT);
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-        // glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+	window = glfwCreateWindow( 640, 480, "Pentagon", NULL, NULL);
+	if( window == NULL ){
+		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible.\n" );
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
 
-        glBegin(GL_TRIANGLES);
-        glColor3f(0.f, 0.f, 1.f);
-        glVertex3f(0.0f, 0.0f, 0.f);
-        glVertex3f(0.5f, 0.0f, 0.f);
-        glVertex3f(0.154508497187f, 0.475528258148f, 0.f);
-        glEnd();
+	// Initialize GLEW
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
 
-        glBegin(GL_TRIANGLES);
-        glColor3f(0.f, 0.f, 1.f);
-        glVertex3f(0.0f, 0.0f, 0.f);
-        glVertex3f(0.154508497187474f, -0.475528258147577f, 0.f);
-        glVertex3f(0.5f, 0.0f, 0.f);
-        glEnd();
+	// Ensure we can capture the escape key being pressed below
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
-        glBegin(GL_TRIANGLES);
-        glColor3f(0.f, 0.f, 1.f);
-        glVertex3f(0.0f, 0.0f, 0.f);
-        glVertex3f(-0.404508497187474f, -0.293892626146237, 0.f);
-        glVertex3f(0.154508497187474f, -0.475528258147577f, 0.f);
-        glEnd();
+	// Green background
+	glClearColor(0.0f, 0.4f, 0.0f, 0.0f);
 
-        glBegin(GL_TRIANGLES);
-        glColor3f(0.f, 0.f, 1.f);
-        glVertex3f(0.0f, 0.0f, 0.f);
-        glVertex3f(-0.404508497187474f, 0.293892626146237, 0.f);
-        glVertex3f(-0.404508497187474f, -0.293892626146237, 0.f);
-        glEnd();
+    // Create VAO
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
 
-        glBegin(GL_TRIANGLES);
-        glColor3f(0.f, 0.f, 1.f);
-        glVertex3f(0.0f, 0.0f, 0.f);
-        glVertex3f(0.154508497187474f, 0.475528258147577f, 0.f);
-        glVertex3f(-0.404508497187474f, 0.293892626146237, 0.f);
-        glEnd();
+    // Pentagon vertices
+    static const GLfloat g_vertex_buffer_data[] = {
+		0.0f, 0.0f, 0.0f,
+		0.5f, 0.0f, 0.0f,
+		0.154508497187474f, 0.475528258147577f, 0.0f,
 
-        // Vertical lines
-        glBegin(GL_LINES);
-        glColor3f(1.f, 1.f, 1.f); // WHITE
-        glVertex3f(0.0f, -480.0f, 0.0f);
-        glVertex3f(0.0f, 480.0f, 0.0f);
-        glEnd();
+		0.0f, 0.0f, 0.0f,
+		0.5f, 0.0f, 0.0f,
+		0.154508497187474f, -0.475528258147577f, 0.0f,
 
-        glBegin(GL_LINES);
-        glColor3f(1.f, 1.f, 1.f); // WHITE
-        glVertex3f(-640.0f, 0.0f, 0.0f);
-        glVertex3f(640.0f, 0.0f, 0.0f);
-        glEnd();
+		0.0f, 0.0f, 0.0f,
+		-0.404508497187474f, -0.293892626146237, 0.0f,
+		0.154508497187474f, -0.475528258147577f, 0.0f,
 
-        // glVertex3f(-0.6f, -0.4f, 0.f);
-        // glColor3f(0.f, 0.f, 1.f);
-        // // glColor3f(0.f, 1.f, 0.f);
-        // glVertex3f(0.6f, -0.4f, 0.f);
-        // glColor3f(0.f, 0.f, 1.f);
-        // // glColor3f(1.f, 0.f, 0.f);
-        // glVertex3f(0.f, 0.6f, 0.f);
-        glEnd();
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-    glfwDestroyWindow(window);
-    glfwTerminate();
-    exit(EXIT_SUCCESS);
+		0.0f, 0.0f, 0.0f,
+		-0.404508497187474f, 0.293892626146237, 0.0f,
+		-0.404508497187474f, -0.293892626146237, 0.0f,
+
+		0.0f, 0.0f, 0.0f,
+		0.154508497187474f, 0.475528258147577f, 0.0f,
+		-0.404508497187474f, 0.293892626146237, 0.0f
+    };
+
+	static const GLfloat g_color_buffer_data[] = {
+		1.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 1.0f,
+
+		1.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+
+		1.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 0.0f,
+
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 1.0f,
+		0.0f, 1.0f, 1.0f,
+
+		1.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 1.0f,
+		1.0f, 0.0f, 1.0f,
+	};
+
+    // Create VBO
+    GLuint vertexbuffer;
+    glGenBuffers(1, &vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+	GLuint colorbuffer;
+	glGenBuffers(1, &colorbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+
+    // Create and compile our GLSL program from the shaders
+    GLuint programID = LoadShaders( "vertexshader.vert", "fragmentshader.frag" );
+
+	do{
+		// Clear the screen.
+		glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+        // Use our shader
+        glUseProgram(programID);
+
+		// 1st attribute buffer : vertices
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(
+			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+        );
+
+		// 2nd attribute buffer : colors
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+        glVertexAttribPointer(
+			1,                  // attribute. No particular reason for 1, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+        );
+
+        // Draw triangles
+        glDrawArrays(GL_TRIANGLES, 0, 5*3); // 5 triangles : 5*3 vertices
+
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+		
+		// Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	} // Check if the ESC key was pressed or the window was closed
+	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+		   glfwWindowShouldClose(window) == 0 );
+
+	// Cleanup VAO, VBO and shader
+	glDeleteBuffers(1, &vertexbuffer);
+	glDeleteBuffers(1, &colorbuffer);
+	glDeleteProgram(programID);
+	glDeleteVertexArrays(1, &VertexArrayID);
+
+	// Close OpenGL window and terminate GLFW
+	glfwTerminate();
+
+	return 0;
 }
