50a41395ad41a09e107251f9cc67ee55df9e5999
ihsansaktia <ihsan.saktia@gmail.com>
2019-03-26 18:51:18 +0700

    Add constructor for kuadran4

--- Week2/Dragon/Image.hpp
+++ Week2/Dragon/Image.hpp
@@ -20,10 +20,12 @@ Image::Image(){
 	vertices.push_back("kuadran1_vertices.txt");
 	vertices.push_back("kuadran2_vertices.txt");
 	// vertices.push_back("kuadran3_vertices.txt");
-	// vertices.push_back("kuadran4_vertices.txt");
+	vertices.push_back("kuadran4_vertices.txt");
 
 	colors.push_back("kuadran1_color.txt");
 	colors.push_back("kuadran2_color.txt");
+	// colors.push_back("kuadran3_color.txt");
+	colors.push_back("kuadran4_color.txt");
 }
 
 vector<string> Image::getVertices(){
