e9d5019698a187622b73731c1aa7eca25936ee8a
ihsansaktia <ihsan.saktia@gmail.com>
2019-04-08 19:26:48 +0700

    Fix bug generating XZ and YZ axis

--- Week3/VertexGenerator.py
+++ Week3/VertexGenerator.py
@@ -1,3 +1,5 @@
+from enum import Enum, auto
+
 class Point():
 
 	def __init__(self, x, y, z):
@@ -7,6 +9,11 @@ class Point():
 
 	def __repr__(self):
 		return "{} {} {}".format(self.x, self.y, self.z)
+
+class Axis(Enum):
+	X = auto()
+	Y = auto()
+	Z = auto()
 
 class VertexGenerator():
 
@@ -23,13 +30,29 @@ class VertexGenerator():
 		separator = self.__createSeparator(3)
 		return "\n{}\n".format(separator).join([lower_half, upper_half])
 
-	def __createSquareWithConstantAxis(self, a1, b1, a2, b2, c):
+	def __createSquareWithConstantAxis(self, a1, b1, a2, b2, c, axis):
 		temp = []
 
-		upper_left = Point(a1, b1, c)
-		upper_right = Point(a2, b1, c)
-		lower_right = Point(a2, b2, c)
-		lower_left = Point(a1, b2, c)
+		upper_left = None
+		upper_right = None
+		lower_right = None
+		lower_left = None
+
+		if axis == Axis.X:
+			upper_left = Point(c, a1, b1)
+			upper_right = Point(c, a2, b1)
+			lower_right = Point(c, a2, b2)
+			lower_left = Point(c, a1, b2)		
+		elif axis == Axis.Y:
+			upper_left = Point(a1, c, b1)
+			upper_right = Point(a2, c, b1)
+			lower_right = Point(a2, c, b2)
+			lower_left = Point(a1, c, b2)	
+		elif axis == Axis.Z:
+			upper_left = Point(a1, b1, c)
+			upper_right = Point(a2, b1, c)
+			lower_right = Point(a2, b2, c)
+			lower_left = Point(a1, b2, c)
 
 		# Lower half of square
 		lower_half = self.__createTriangle(upper_left, lower_left, lower_right)
@@ -40,13 +63,13 @@ class VertexGenerator():
 		return self.__createSquare(lower_half, upper_half)
 
 	def __createSquareAlongXYAxis(self, x1, y1, x2, y2, z):
-		return self.__createSquareWithConstantAxis(x1, y1, x2, y2, z)
+		return self.__createSquareWithConstantAxis(x1, y1, x2, y2, z, Axis.Z)
 
 	def __createSquareAlongXZAxis(self, x1, z1, x2, z2, y):
-		return self.__createSquareWithConstantAxis(x1, z2, x2, z1, y)
+		return self.__createSquareWithConstantAxis(x1, z2, x2, z1, y, Axis.Y)
 
 	def __createSquareAlongYZAxis(self, y1, z1, y2, z2, x):
-		return self.__createSquareWithConstantAxis(y1, z1, y2, z2, x)
+		return self.__createSquareWithConstantAxis(y1, z1, y2, z2, x, Axis.X)
 
 	def __createCube(self, x1, y1, z1, x2, y2, z2):
 		separator = self.__createSeparator(10)
@@ -54,8 +77,8 @@ class VertexGenerator():
 		front = self.__createSquareAlongXYAxis(x1, y1, x2, y2, z1)
 		back = self.__createSquareAlongXYAxis(x1, y1, x2, y2, z2)
 
-		top = self.__createSquareAlongXZAxis(x1, z2, x2, z1, y1)
-		bottom = self.__createSquareAlongXZAxis(x1, z2, x2, z1, y2)
+		top = self.__createSquareAlongXZAxis(x1, z1, x2, z2, y1)
+		bottom = self.__createSquareAlongXZAxis(x1, z1, x2, z2, y2)
 
 		left = self.__createSquareAlongYZAxis(y1, z1, y2, z2, x1)
 		right = self.__createSquareAlongYZAxis(y1, z1, y2, z2, x2)
