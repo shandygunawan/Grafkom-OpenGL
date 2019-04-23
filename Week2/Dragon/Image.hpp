c0daf1ef983bd2c300542fb8675bc986341a33c4
Ihsan M. A <31171384+ihsansaktia@users.noreply.github.com>
2019-04-23 19:21:46 +0700

    Week2 (#2)
    * Add placeholder for week2
    
    * Copy and paste week1 to pentagon week2
    
    * Updated pentagon with different colors for each vertex
    
    * convert codes to OOP, fix bugs, add ability to read files for input, add IOController & State Class & GlobalLib
    
    * change input mode, add ability to read create VBOs from multiple files
    
    * fix create vertices from files
    
    * add Image class, able to read multiple files
    
    * update screen size
    
    * bug fix: add different layer for color
    
    * change resolution to 1:1
    
    * Changed input to grid units
    
    * bug fix: add multiple files to one vbo instead of multiple vbos
    
    * bug fix: add multiple files to one vbo instead of multiple vbos
    
    * kuadran 1 & 2 finished
    
    * kuadran 3 finished
    
    * Changed grid size to 20
    
    * Add constructor for kuadran4
    
    * Add stripes for kuadran4
    
    * Add neck and shoulder
    
    * Add half body
    
    * Done body
    
    * Add color and vertex generator
    
    * Add body with color
    
    * Add leg
    
    * Done dragon

---
+++ Week2/Dragon/Image.hpp
@@ -0,0 +1,39 @@
+#ifndef _IMAGE_
+#define _IMAGE_
+
+#include <vector>
+#include <string>
+
+class Image {
+private:
+	vector<string> vertices;
+	vector<string> colors;
+
+public:
+	Image();
+
+	vector<string> getVertices();
+	vector<string> getColors();
+};
+
+Image::Image(){
+	vertices.push_back("kuadran1_vertices.txt");
+	vertices.push_back("kuadran2_vertices.txt");
+	vertices.push_back("kuadran3_vertices.txt");
+	vertices.push_back("kuadran4_vertices.txt");
+
+	colors.push_back("kuadran1_color.txt");
+	colors.push_back("kuadran2_color.txt");
+	colors.push_back("kuadran3_color.txt");
+	colors.push_back("kuadran4_color.txt");
+}
+
+vector<string> Image::getVertices(){
+	return vertices;
+}
+
+vector<string> Image::getColors(){
+	return colors;
+}
+
+#endif
